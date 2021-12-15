ecfc0f91eee1728f50bbf43620f5b17379647ecd
Raúl García-Verdugo Cardador <60638702+Zerronir@users.noreply.github.com>
2021-12-15 01:02:51 +0100

    all game

--- Program.cs
+++ Program.cs
@@ -28,23 +28,22 @@ namespace c_sharp
                 if(i > num) {
                     chances--;
 
-                    Console.WriteLine("El número introducido es superior... te quedan " + chances + " intentos");
+                    Console.WriteLine("Too high, guess lower, you still have " + chances + " chances left");
                 } else if(i < num) {
                     chances--;
-                    Console.WriteLine("El número introducido es inferior... te quedan " + chances + " intentos");
+                    Console.WriteLine("Too low, guess higher, you still have " + chances + " chances left");
                 } else if(chances == 0) {
-                    Console.WriteLine("Has perdido el juego, el número correcto era " + num);
-                    break;
-                } else if(i == num) {
-                    Console.WriteLine("Has adivinado el número!!! Lo has hecho en: " + loops + " intentos");
+                    Console.WriteLine("You loose, the correct number was... " + num);
                     control = true;
-                    break;
+                } else if(i == num) {
+                    Console.WriteLine("YAAAY YOU WON!!! Guessed in: " + loops + " chances");
+                    control = true;
                 }
 
 
             } while(control == false);
             Console.WriteLine();
-            Console.Write("Para finalizar presiona cualquier tecla");
+            Console.Write("To finish, please press any key to continue");
             Console.ReadKey(true);
         }
     }
